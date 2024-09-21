c2496f162a7c58bf1a1dc8e020476d517694148f
Sahil More <skm842011@gmail.com>
2024-09-22 02:11:00 +0530

    Create First_index_of _a_substring.py

---
+++ First_index_of _a_substring.py
@@ -0,0 +1,25 @@
+import cProfile
+
+def first_index(my_string, sub_string):
+    my_string = my_string.lower()                  #FOUR TIMES FASTER THAN USUAL CODE
+    sub_string = sub_string.lower()
+    len_of_substr = len(sub_string)
+    len_of_mystr = len(my_string)
+
+    for search in range(0, len_of_mystr - len_of_substr + 1, 1):
+        if my_string[search] == sub_string[0]:
+            for sub_search in range(1, len_of_substr, 1):
+                if sub_string[sub_search] == my_string[search + sub_search]:
+                    continue
+                else:
+                    break
+            else:
+                return search
+            continue
+        else:
+            continue
+    return "Substring not found"
+  
+test_str = "a" * 1000000 + "hell" + "b" * 100000000  + 'hello' + 'z'
+sub_str = "z"
+cProfile.run("first_index(test_str,sub_str)")
